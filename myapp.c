9f31606b4d65e34fdd96feeb3ef1d010adbd2d46
sazan10 <33653716+sazan10@users.noreply.github.com>
2024-09-19 00:43:47 +0545

    Create myapp.c

---
+++ myapp.c
@@ -0,0 +1,32 @@
+include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_THREADS 100
+
+void* thread_func(void* arg) {
+    printf("Thread %ld is running\n", (long)arg);
+    // Simulate some work with a sleep
+    sleep(1);
+    return NULL;
+}
+
+int main() {
+    pthread_t threads[NUM_THREADS];
+
+    // Create threads
+    for (long i = 0; i < NUM_THREADS; i++) {
+        if (pthread_create(&threads[i], NULL, thread_func, (void*)i)) {
+            fprintf(stderr, "Error creating thread %ld\n", i);
+            return 1;
+        }
+    }
+
+    // Wait for threads to finish
+    for (long i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("All threads completed.\n");
+    return 0;
+}
